0f06491a5cf481652be19acd039ceea7bc2ed08f
JoshuaPalmier <69690552+NashuaOM@users.noreply.github.com>
2021-03-05 11:21:53 -0700

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,83 @@
+
+// // assignment 1
+
+// // List of 20 variables
+// var numberofpeople = 20;
+// var name1 = "josh";
+// var name2 ="john";      
+// var name3 ="mark";  
+// var name4 ="kenny"; 
+// var name5 ="smith"; 
+// var name6 ="luke";  
+// var name7 ="martin";    
+// var name8 ="dewayne";   
+// var name9 ="johnson";   
+// var name10 ="marvin";   
+// var name11 ="chris";        
+// var name12 ="campbell"; 
+// var name13 ="steven";   
+// var name14 ="larry";                
+// var name15 ="ben";  
+// var name16 ="peter";    
+// var name17 ="maryy";    
+// var name18 ="tyson";    
+// var name19 ="nick"; 
+// var name20 ="tony";
+// // used document.writein to display JS on HTML 
+// document.writeln(` I have ${numberofpeople} people to list ${name1},${name2},${name3},${name4},${name5},${name6},${name7},${name8},${name9},${name10},${name11},${name12},${name13},${name14}${name15},${name16},${name17},${name18},${name19},${name20},.`);
+// // used this  to log variables to the console.
+// console.log(` I have ${numberofpeople} people to list ${name1},${name2},${name3},${name4},${name5},${name6},${name7},${name8},${name9},${name10},${name11},${name12},${name13},${name14}${name15},${name16},${name17},${name18},${name19},${name20},.`);
+
+// // for loop
+
+// for(var i=1;i<=100;i+=2) {
+// console.log(i);
+// }
+
+// let theNumber =number(prompt("pick a number"));
+// if (!number. isNAN(theNUmber)) {
+//     console.log("Your Number is the square root of  " + theNumber )
+// }
+
+
+
+
+
+
+
+// // if statement log in the console.
+// if (name1 = "josh") {
+//     console.log(`${name1} you have an appointment today`);
+// }
+
+// else {
+//     console.log(`how are you?`);
+// }
+
+
+// let result = "*";
+
+// for (var count="*";count< 10; count++ )
+//  result= result++;
+//  console.log(result);
+
+
+
+
+// Assignment 2
+var productName= prompt("what is the products name?");
+var productProgram= prompt("what is the products program?");
+var productQuality=prompt("what is the products Quality?");
+var productPrice=prompt("what is the products price?");
+
+console.log(productName);
+console.log(productProgram);
+console.log(Number(productQuality));
+console.log (Number(productPrice));
+console.log(productQuality + productPrice *.16);
+
+
+
+
+
+
